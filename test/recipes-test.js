1de5d88ac4fe524772ae199d0038c304e1812413
Willem Pol <wpol.edu@gmail.com>
2024-07-08 09:16:00 -0600

    started building recipe functions, began testing

--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,8 +1,18 @@
 import { expect } from 'chai';
 import { findRecipeIngredients } from '../src/recipes';
+import recipeData from '../src/data/recipes'
 
 describe('Recipe', () => {
   it('Should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
   });
+
+  it('should find the list of ingredients for a given recipe', () => {
+    let recipeID = 595736
+    let ingredientsControlList = [20081, 18372, 1123, 19335, 19206, 19334, 2047, 1012047, 10019903, 1145, 2050]
+
+    let ingredientsList = findRecipeIngredients(recipeData, recipeID)
+
+    expect(ingredientsList).to.deep.equal(ingredientsControlList)
+  })
 })
