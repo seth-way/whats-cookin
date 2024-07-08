fd515cbf6f73b95b5a384e4e130cb721a5f47d99
Willem Pol <wpol.edu@gmail.com>
2024-07-08 09:31:26 -0600

    slight formatting adjustment in testing

--- test/recipes-test.js
+++ test/recipes-test.js
@@ -2,8 +2,8 @@ import { expect } from 'chai';
 import { findRecipeIngredients } from '../src/recipes';
 import recipeData from '../src/data/recipes'
 
-describe('Recipe', () => {
-  it('Should be a function', () => {
+describe('find ingredients', () => {
+  it('should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
   });
 
