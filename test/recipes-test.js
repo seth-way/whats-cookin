cc5b0eb1def48e0e591a424fff588b4326f5e7cc
Willem Pol <wpol.edu@gmail.com>
2024-07-08 18:30:49 -0600

    created ingredients.js for functions, began testing
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Seth Way <way.sethaniel@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>

--- test/recipes-test.js
+++ test/recipes-test.js
@@ -2,17 +2,18 @@ import { expect } from 'chai';
 import { findRecipeIngredients } from '../src/recipes';
 import recipeData from '../src/data/recipes'
 
+
 describe('find ingredients', () => {
   it('should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
-  });
-
-  it('should find the list of ingredients for a given recipe', () => {
-    let recipeID = 595736
-    let ingredientsControlList = [20081, 18372, 1123, 19335, 19206, 19334, 2047, 1012047, 10019903, 1145, 2050]
-
-    let ingredientsList = findRecipeIngredients(recipeData, recipeID)
-
-    expect(ingredientsList).to.deep.equal(ingredientsControlList)
-  })
+    });
+    
+    it('should find the list of ingredient ids for a given recipe', () => {
+      let recipeID = 595736
+      let ingredientIDsControlList = [20081, 18372, 1123, 19335, 19206, 19334, 2047, 1012047, 10019903, 1145, 2050]
+      
+      let ingredientIDsList = findRecipeIngredients(recipeData, recipeID)
+      
+      expect(ingredientIDsList).to.deep.equal(ingredientIDsControlList)
+    })
 })
