59de5698b7a9ee42d04c1618fd7e1625160f2063
Seth Way <way.sethaniel@gmail.com>
2024-07-09 19:47:27 -0400

    create functions to filter by name & tag, & return instructions for a recipe
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>
    Co-authored-by: Willem Pol <wavpool@gmail.com>

--- src/recipes.js
+++ src/recipes.js
@@ -1,13 +1,24 @@
-//Here is an example demonstrating logic separated that can be imported into the scripts and test files. Feel free to update this later!
 import { estimateCostPerIngredient } from './ingredients';
-import ingredientsData from './data/ingredients';
 
 export const findRecipeIngredients = (recipesList, recipeID) => {
-  let recipe = recipesList.find(currentRecipe => currentRecipe.id === recipeID);
-  let recipeIngredients = recipe.ingredients;
-  let ingredientList = recipeIngredients.map(ingredient => ingredient.id);
+  const recipe = recipesList.find(
+    currentRecipe => currentRecipe.id === recipeID
+  );
+  const recipeIngredients = recipe.ingredients;
+  const ingredientList = recipeIngredients.map(ingredient => ingredient.id);
 
   return ingredientList;
+};
+
+export const findRecipeInstructions = (recipesList, recipeID) => {
+  const recipe = recipesList.find(
+    currentRecipe => currentRecipe.id === recipeID
+  );
+  const recipeInstructions = recipe.instructions.sort(
+    (a, b) => a.number - b.number
+  );
+
+  return recipeInstructions.map(instructionData => instructionData.instruction);
 };
 
 export const estimateCostPerRecipeIngredients = (ingredientsData, recipe) => {
@@ -18,4 +29,14 @@ export const estimateCostPerRecipeIngredients = (ingredientsData, recipe) => {
 
 export const estimateCostPerRecipe = ingredientCosts => {
   return ingredientCosts.reduce((acc, val) => (acc += val), 0);
+};
+
+export const filterRecipesByTag = (recipesList, tag) => {
+  return recipesList.filter(recipe => recipe.tags.includes(tag));
+};
+
+export const filterRecipesByName = (recipesList, tag) => {
+  return recipesList.filter(recipe =>
+    recipe.name.toLowerCase().includes(tag.toLowerCase())
+  );
 };
--- test/recipes-test.js
+++ test/recipes-test.js
@@ -1,27 +1,53 @@
 import { expect } from 'chai';
 import {
   findRecipeIngredients,
+  findRecipeInstructions,
   estimateCostPerRecipe,
   estimateCostPerRecipeIngredients,
+  filterRecipesByTag,
+  filterRecipesByName,
 } from '../src/recipes';
 import recipeData from '../src/data/recipes';
 import ingredientsData from '../src/data/ingredients';
 
-describe('find ingredients', () => {
+describe("find a recipe's ingredients", () => {
   it('should be a function', () => {
     expect(findRecipeIngredients).to.be.a('function');
   });
 
   it('should find the list of ingredient ids for a given recipe', () => {
-    let recipeID = 595736;
-    let ingredientIDsControlList = [
+    const recipeID = 595736;
+    const ingredientIDsControlList = [
       20081, 18372, 1123, 19335, 19206, 19334, 2047, 1012047, 10019903, 1145,
       2050,
     ];
 
-    let ingredientIDsList = findRecipeIngredients(recipeData, recipeID);
+    const ingredientIDsList = findRecipeIngredients(recipeData, recipeID);
 
     expect(ingredientIDsList).to.deep.equal(ingredientIDsControlList);
+  });
+});
+
+describe("find a recipe's instructions", () => {
+  it('should be a function', () => {
+    expect(findRecipeInstructions).to.be.a('function');
+  });
+
+  it('should find the list of ingredient ids for a given recipe', () => {
+    const recipeID = 741603;
+    const controlInstructions = [
+      'Watch how to make this recipe.',
+      'In a large bowl, whisk together buttermilk, eggs, baking powder, sugar, salt and butter.',
+      'In another large bowl mix together all-purpose flour and buckwheat flour.',
+      'Slowly add flour into the wet ingredients mixing with a whisk.',
+      'Mix until there are no lumps and the batter is smooth and velvety.',
+      'In a large cast iron skillet or flat grill pan over medium-high heat, melt a tablespoon of butter. Ladle pancake batter onto skillet to desired size. Using the ladle, form pancake into circular shape. Cook on each side for 2 to 3 minutes or until golden brown. Set pancakes aside and keep warm. Repeat with remaining ingredients.',
+      'Once completed, spread peanut butter on a pancake, layer it with sliced bananas and drizzle it with honey. Top the pancake with another pancake to form a sandwich. Repeat with remaining pancakes and serve with extra honey.',
+    ];
+
+    const resultInstructions = findRecipeInstructions(recipeData, recipeID);
+
+    expect(controlInstructions).to.deep.equal(resultInstructions);
   });
 });
 
@@ -59,5 +85,45 @@ describe('estimate recipe total cost', () => {
     const recipeTotal = estimateCostPerRecipe(testRecipeIngredientCosts);
 
     expect(recipeTotal).to.equal(testTotal);
-  })
+  });
+});
+
+describe('filter recipes list by tag', () => {
+  it('should be a function', () => {
+    expect(filterRecipesByTag).to.be.a('function');
+  });
+
+  it('should filter recipes list to only include those with the given tag', () => {
+    const filteredRecipes = filterRecipesByTag(recipeData, 'antipasti');
+
+    expect(filteredRecipes.length).to.equal(9);
+  });
+
+  it('should work on any tag', () => {
+    const filteredRecipes = filterRecipesByTag(recipeData, 'antipasti');
+    const filteredRecipes2 = filterRecipesByTag(recipeData, 'lunch');
+
+    expect(filteredRecipes.length).to.equal(9);
+    expect(filteredRecipes2.length).to.equal(12);
+  });
+});
+
+describe('filter recipes list by recipe name', () => {
+  it('should be a function', () => {
+    expect(filterRecipesByName).to.be.a('function');
+  });
+
+  it('should filter recipes list to only include those whose name includes given string', () => {
+    const filteredRecipes = filterRecipesByName(recipeData, 'cookie');
+
+    expect(filteredRecipes.length).to.equal(6);
+  });
+
+  it('should work on any name', () => {
+    const filteredRecipes = filterRecipesByName(recipeData, 'cookie');
+    const filteredRecipes2 = filterRecipesByName(recipeData, 'salad');
+
+    expect(filteredRecipes.length).to.equal(6);
+    expect(filteredRecipes2.length).to.equal(4);
+  });
 });
