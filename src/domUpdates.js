f55050875bf0d101b142596c12eadb0e08e73376
Carissa Hluchan <carissahluchan@gmail.com>
2024-07-09 19:41:07 -0600

    Add: Basic HTML & CSS
    Co-authored-by: Seth Way <way.sethaniel@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>
    Co-authored-by: Willem Pol <wavpool@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,6 +1,8 @@
 //NOTE: Your DOM manipulation will occur in this file
 
-//Here is an example function just to demonstrate one way you can export/import between the two js files. You'll want to delete this once you get your own code going.
+//Here is an example function just to demonstrate one way 
+// you can export/import between the two js files. 
+// You'll want to delete this once you get your own code going.
 const displayRecipes = () => {
   console.log(`Displaying recipes now`)
 }
