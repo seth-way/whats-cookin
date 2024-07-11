56ff22752a027e95ae775a74c8c86c14647a3302
Marshall Hotaling <marshall.hotaling@gmail.com>
2024-07-10 19:17:47 -0600

    dom functionality
    created function and styling to show recipes on the DOM
    
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Seth Way <way.sethaniel@gmail.com>
    Co-authored-by: Willem Pol <wavpool@gmail.com>

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -1,13 +1,58 @@
-//NOTE: Your DOM manipulation will occur in this file
+import recipeData from "./data/recipes";
 
+
+
+
+
+
+
+//NOTE: Your DOM manipulation will occur in this file
+var recipeContainer = document.querySelector(".recipe-list");
 //Here is an example function just to demonstrate one way 
 // you can export/import between the two js files. 
 // You'll want to delete this once you get your own code going.
-const displayRecipes = () => {
+
+window.addEventListener("load",() =>displayRecipes(recipeData,recipeContainer));
+// make an call back function to envoke multiple function's on page load
+
+const displayRecipes = (recipeList,recipeContainer) => {
+  recipeContainer.innerHTML = ""
+  recipeList.forEach(recipe => {
+    const recipeCard = createRecipeCard(recipe)
+    recipeContainer.appendChild(recipeCard)
+
+
+
+  })
   console.log(`Displaying recipes now`)
 }
 
 
+
+function createRecipeCard(recipe, recipeContainer) {
+  const recipeCard = document.createElement("figure");
+  recipeCard.setAttribute("class", "recipe-card");
+  // recipeContainer.appendChild(recipeCard);
+  const recipeTitle = document.createElement("figcaption");
+  recipeTitle.innerText = recipe.name;
+  recipeCard.appendChild(recipeTitle);
+  const recipeImage = createImage(recipe.image, `Image of ${recipe.name} dish`);
+  recipeCard.appendChild(recipeImage);
+  return recipeCard
+}
+
+
+
+function createImage(imageSource, imageAlt) {
+  const recipeImg = document.createElement("img");
+  recipeImg.src = imageSource;
+  recipeImg.alt = imageAlt;
+  return recipeImg;
+}
+
+
+
+
 export {
-  displayRecipes,
+  displayRecipes,createRecipeCard
 }
