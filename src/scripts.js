0bd9b4b82908d172c67e5f56010cdce476efe52e
Seth Way <way.sethaniel@gmail.com>
2024-07-11 18:45:56 -0400

    create functions to produce recipe cards from data
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>
    Co-authored-by: Willem Pol <wavpool@gmail.com>

--- src/scripts.js
+++ src/scripts.js
@@ -1,21 +1,24 @@
 //NOTE: Data model and non-dom manipulating logic will live in this file.
 
-import './styles.css'
-import apiCalls from './apiCalls'
+import './styles.css';
+import apiCalls from './apiCalls';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
-import './images/hamburger.svg'
-import ingredientsData from './data/ingredients'
+import './images/turing-logo.png';
+import './images/hamburger.svg';
+import ingredientsData from './data/ingredients';
 // Below are examples of how you can import functions from either the recipes or domUpdates files.
 import { findRecipeIngredients } from './recipes';
-import { displayRecipes } from './domUpdates'
+import { displayRecipes } from './domUpdates';
 
-console.log(ingredientsData)
+console.log(ingredientsData);
 // findRecipeIngredients("Dirty Steve's Original Wing Sauce")
 // displayRecipes();
 
 window.addEventListener('load', () => {
-    var drawer = document.querySelector('#recipeDrawer')
-    document.body.addEventListener('click', () => { drawer.classList.toggle('unhide') })
-    console.log('drawer: ',drawer)
-})
+  var drawer = document.querySelector('#recipeDrawer');
+  drawer.style.height = '100%';
+  document.body.addEventListener('click', () => {
+    drawer.classList.toggle('unhide');
+  });
+  console.log('drawer: ', drawer);
+});
