fe492c504b7ffc5677d29347eb85c2c35839deed
Seth Way <way.sethaniel@gmail.com>
2024-07-12 18:57:01 -0400

    finish feature recipe drawer update on user click
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>
    Co-authored-by: Willem Pol <wavpool@gmail.com>

--- src/ingredients.js
+++ src/ingredients.js
@@ -1,11 +1,25 @@
-export const getIngredientNames = (ingredientsData, recipeIDs) => {
-  const recipeNameList = recipeIDs.map(id => {
+export const getIngredientNames = (ingredientsData, ingredientIDs) => {
+  const ingredientNamesList = ingredientIDs.map(id => {
     let currentIngredient = ingredientsData.find(
       ingredient => ingredient.id === id
     );
     return currentIngredient.name;
   });
-  return recipeNameList;
+  return ingredientNamesList;
+};
+
+export const getIngredientsInfo = (ingredientsData, ingredients) => {
+  const updatedIngredients = ingredients.map(ingredient => {
+    const ingredientInfo = ingredientsData.find(
+      currentIngredient => currentIngredient.id === ingredient.id
+    );
+    ingredient.name = ingredientInfo.name;
+    ingredient.estimatedCostInCents = ingredientInfo.estimatedCostInCents;
+    
+    return ingredient;
+  });
+
+  return updatedIngredients;
 };
 
 export const estimateCostPerIngredient = (
--- src/scripts.js
+++ src/scripts.js
@@ -1,19 +1,23 @@
-//NOTE: Data model and non-dom manipulating logic will live in this file.
-
 import './styles.css';
 import apiCalls from './apiCalls';
 // --- // Images // --- //
 import './images/turing-logo.png';
 import './images/hamburger.svg';
-// --- // Images // --- //
+// --- // Data // --- //
+import recipeData from './data/recipes';
 import ingredientsData from './data/ingredients';
-// Below are examples of how you can import functions from either the recipes or domUpdates files.
-import { findRecipeIngredients } from './recipes';
-import { displayRecipes } from './domUpdates';
-
+// --- // Methods // --- //
+import { displayRecipes, updateFeaturedRecipe } from './domUpdates';
+import { findRecipe } from './recipes';
+// --- // Variables // --- //
+var featuredRecipe = {};
+var recipes = [];
+var filteredRecipes = [];
+// --- // DOM Nodes // --- //
+// -- containers -- //
 const recipesContainer = document.querySelector('.recipes-container');
-const featuredRecipe = document.getElementById('featured-recipe');
-// --- // Buttons // --- //
+const featuredRecipeContainer = document.getElementById('featured-recipe');
+// -- buttons -- //
 const closeFeaturedRecipeBtn = document.getElementById('close-featured-recipe');
 const toTopBtn = document.getElementById('to-top');
 const toBottomBtn = document.getElementById('to-bottom');
@@ -21,16 +25,24 @@ const toBottomBtn = document.getElementById('to-bottom');
 // findRecipeIngredients("Dirty Steve's Original Wing Sauce")
 // displayRecipes();
 // --- // Event Listeners // --- //
+window.addEventListener('load', () => {
+  recipes = recipeData; // later this will be a data fetch
+  displayRecipes(recipes, recipesContainer);
+});
+
 recipesContainer.addEventListener('click', event => {
   const recipeCard = event.target.closest('figure');
   if (recipeCard) {
-    // must create a recipe card and populate featured recipe section with clicked card
-    featuredRecipe.classList.add('unhide');
+    const recipeId = Number(recipeCard.id);
+    const recipe = findRecipe(recipes, recipeId);
+    featuredRecipe = { ...recipe };
+    updateFeaturedRecipe(featuredRecipe);
+    featuredRecipeContainer.classList.add('unhide');
   }
 });
 
 closeFeaturedRecipeBtn.addEventListener('click', () => {
-  featuredRecipe.classList.remove('unhide');
+  featuredRecipeContainer.classList.remove('unhide');
 });
 
 toTopBtn.addEventListener('click', () => {
@@ -46,3 +58,5 @@ toBottomBtn.addEventListener('click', () => {
     behavior: 'smooth',
   });
 });
+
+recipeData.reduce((tags, tag))
