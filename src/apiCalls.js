5bd1ee64aee49bee728a84046510c585b0ee03a0
Willem Pol <wpol.edu@gmail.com>
2024-07-22 18:50:30 -0600

    updated GET request to refer to local server, built a POST request
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Seth Way <way.sethaniel@gmail.com

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,32 @@
 export const fetchData = type => {
   const data = fetch(
-    `https://what-s-cookin-starter-kit.herokuapp.com/api/v1/${type}`
+    `http://localhost:3001/api/v1/${type}`
   )
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`GET ERROR: Response not OK >>> STATUS ${response.status} - ${response.statusText}`)
+      }
+      return response.json()
+    })
     .then(data => data)
-    .catch(error => console.log(error));
+    .catch(error => alert(`GET ERROR >>> STATUS ${error.status} - ${error.statusText}`));
   return data;
 };
+
+export const postData = (type, resource) => {
+  return fetch(`http://localhost:3001/api/v1/${type}`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(resource),
+  })
+    .then(response => {
+      if (response.status !== 201) {
+        throw new Error(`POST ERROR: Resource not created >>> STATUS ${response.status} - ${response.statusText}`)
+      }
+      return response.json()
+    })
+    .then(data => data)
+    .catch(error => alert(`POST ERROR >>> STATUS ${error.status} - ${error.statusText}`))
+}
