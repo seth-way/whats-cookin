e4e197f2d49a2c025d9742ba29831bdd57412b31
Seth Way <way.sethaniel@gmail.com>
2024-07-24 19:19:46 -0400

    add sliders for filtering recipes by cost
    Co-authored-by: Carissa Hluchan <CarissaHluchan@gmail.com>
    Co-authored-by: Marshall Hotaling <marshall.hotaling@gmail.com>
    Co-authored-by: Willem Pol <wavpool@gmail.com>

---
+++ src/costFilterSliders.js
@@ -0,0 +1,49 @@
+export const createCostFilterSliders = maxCost => {
+  // let rangeMin = 0;
+  // let rangeMax = maxCost;
+
+  const minCostTextInput = document.getElementById('min-cost-filter');
+  const maxCostTextInput = document.getElementById('max-cost-filter');
+  const rangeInputTrack = document.getElementById('cost-range-track');
+  const rangeInputSliders = document.querySelectorAll(
+    '.cost-range-slider-box .double-range-slider input'
+  );
+
+  const minRangeFill = () => {
+    rangeInputTrack.style.left =
+      (rangeInputSliders[0].value / rangeInputSliders[0].max) * 100 + '%';
+  };
+
+  const maxRangeFill = () => {
+    rangeInputTrack.style.right =
+      100 - (rangeInputSliders[1].value / rangeInputSliders[1].max) * 100 + '%';
+  };
+
+  const setMinCostOutput = () => {
+    minCostTextInput.value = rangeInputSliders[0].value;
+    return parseInt(rangeInputSliders[0].value);
+  };
+  const setMaxCostOutput = () => {
+    maxCostTextInput.value = rangeInputSliders[1].value;
+    return parseInt(rangeInputSliders[1].value);
+  };
+  // initiate sliders
+  rangeInputSliders.forEach(input => {
+    input.setAttribute('max', maxCost);
+  });
+
+  rangeInputSliders[1].value = maxCost;
+  setMinCostOutput();
+  setMaxCostOutput();
+  minRangeFill();
+  maxRangeFill();
+  // slider event listener callback
+  
+
+  return {
+    minRangeFill,
+    maxRangeFill,
+    setMinCostOutput,
+    setMaxCostOutput,
+  };
+};
