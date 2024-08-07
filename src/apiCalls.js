438e61209fd51f672185cba5fa8d482366dfef52
Seth Way <way.sethaniel@gmail.com>
2024-08-07 12:12:14 -0400

    redirect api calls to remote url

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,32 +1,40 @@
+const baseURL = 'https://whats-cookin-api-bay.vercel.app/';
+
 export const fetchData = type => {
-  const data = fetch(
-    `http://localhost:3001/api/v1/${type}`
-  )
+  const data = fetch(`${baseURL}api/v1/${type}`)
     .then(response => {
       if (!response.ok) {
-        throw new Error(`GET ERROR: Response not OK >>> STATUS ${response.status} - ${response.statusText}`)
+        throw new Error(
+          `GET ERROR: Response not OK >>> STATUS ${response.status} - ${response.statusText}`
+        );
       }
-      return response.json()
+      return response.json();
     })
     .then(data => data)
-    .catch(error => alert(`GET ERROR >>> STATUS ${error.status} - ${error.statusText}`));
+    .catch(error =>
+      alert(`GET ERROR >>> STATUS ${error.status} - ${error.statusText}`)
+    );
   return data;
 };
 
 export const postData = (endpoint, resource) => {
-  return fetch(`http://localhost:3001/api/v1/${endpoint}`, {
+  return fetch(`${baseURL}api/v1/${endpoint}`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(resource),
   })
     .then(response => {
       if (response.status !== 201) {
-        throw new Error(`POST ERROR: Resource not created >>> STATUS ${response.status} - ${response.statusText}`)
+        throw new Error(
+          `POST ERROR: Resource not created >>> STATUS ${response.status} - ${response.statusText}`
+        );
       }
-      return response.json()
+      return response.json();
     })
     .then(data => data)
-    .catch(error => alert(`POST ERROR >>> STATUS ${error.status} - ${error.statusText}`))
-}
+    .catch(error =>
+      alert(`POST ERROR >>> STATUS ${error.status} - ${error.statusText}`)
+    );
+};
